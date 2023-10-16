5b097e7c26b76b8f2275ebab79e5c3441097088b
entsoereng <entsoereng@aol.com>
2023-10-16 15:34:52 +0200

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print alphabets in reverse.
+* Return: alphabets in reverse
+*/
+int main(void)
+{
+char l;
+for (l = 'z'; l => 'a'; l--)
+putchar(l);
+Putchar('\n');
+return (0);
+}
