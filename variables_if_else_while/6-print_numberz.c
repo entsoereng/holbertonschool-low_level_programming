4afe87d15c1d6610551161e04da47ce8df3f02ec
entsoereng <entsoereng@aol.com>
2023-10-16 15:27:17 +0200

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Print single base 10 numbers.
+* Return: base 10 single numbers.
+*/
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
