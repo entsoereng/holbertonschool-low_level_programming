842a1c735df105cb1ddbbfeb4ba4ac90781b615e
entsoereng <entsoereng@aol.com>
2023-10-16 15:53:39 +0200

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Print base 16 numbers in lowercase
+* Return: base 16 numbers in lowercase.
+*/
+int main(void)
+{
+int num;
+char l;
+for(num = '0'; num <= '9'; num++)
+putchar(num);
+for(l = 'a'; l <= 'z'; l++)
+putchar(l);
+putchar('\n');
+return (0);
+}
