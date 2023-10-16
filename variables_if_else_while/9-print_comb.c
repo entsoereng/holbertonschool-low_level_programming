ed622bdaabf3411cf724d89aaef6a0dacb51b9d5
entsoereng <entsoereng@aol.com>
2023-10-16 16:10:15 +0200

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - print all possible combinations of single digit numbers.
+* Return: All possible combinations of single digit numbers.
+*/
+int main(void)
+{
+int num;
+for (num = '0'; num , <= '9'; num++)
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
