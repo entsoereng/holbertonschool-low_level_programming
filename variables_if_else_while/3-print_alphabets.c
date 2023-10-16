4acc205cf4a5e34b3fbb8e6968a892c897ca6c8e
entsoereng <entsoereng@aol.com>
2023-10-16 14:29:20 +0200

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints alphabetes in lower case and in upper case.
+* Return: Upper case or Lower case.
+*/
+int main(void)
+{
+  char low;
+for (low = 'a'; low <= 'z'; low++)
+  putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+  putchar(low);
+putchar("\n");
+return (0);
+}
