f4b89f43b8ae48c907513963b7fa3f18f58aae4b
entsoereng <entsoereng@aol.com>
2023-10-16 15:17:32 +0200

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
 * main - Print all single digit numbers of base 10 starting from 0.
-* Return single numbers.
+* Return: single numbers of base 10 starting from 0.
 */
 int main(void)
 {
