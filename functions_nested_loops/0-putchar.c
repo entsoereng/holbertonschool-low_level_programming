b119ab683f3a4e51858cb751ec9a2bd4f8f1cfb8
entsoereng <entsoereng@aol.com>
2023-10-17 12:38:49 +0200

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,1 +1,14 @@
-int _putchar(char c)
+#include <main.h>
+/**
+* main - Print _putchar
+* Return: 
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('w');
+_putchar('t');
+_putchar('h');
+_putchar('\n');
+return (0);
+}
