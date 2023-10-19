a50d1f0b02779c621bd05b808452c278b01865e4
entsoereng <entsoereng@aol.com>
2023-10-19 16:12:53 +0200

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifindef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-
+/**
+* main - Print alphabet, lower case on one line
+*/
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,4 +13,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif
