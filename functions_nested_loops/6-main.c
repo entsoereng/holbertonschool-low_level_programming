d8a8d0557a5b4cbceb96ee98671977126007a0a9
entsoereng <entsoereng@aol.com>
2023-10-23 05:46:23 -0700

    nested loops

--- functions_nested_loops/6-main.c
+++ functions_nested_loops/6-main.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
-
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 
 int main(void)
 
 {
 int r;
+
 r = _abs(-1);
 printf("%d\n", r);
 r = _abs(0);
