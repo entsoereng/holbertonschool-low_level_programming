45b7d4c548dd04c5b21f4c76b8ea3a8425629a6c
entsoereng <entsoereng@aol.com>
2023-10-23 06:47:43 -0700

    print_alphabet_x10

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from
- * Return: Always 0
- */
+* print_to_98 - print numbers n to 98
+* @n: integer argument
+*/
+
 void print_to_98(int n)
 {
-if (n <= 98)
-{
-for (; n <= 98; n++)
-{
-if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
-else
+while (n < 98)
 {
 printf("%d, ", n);
-}
-}
-}
-else
-{
-for (; n >= 98; n--)
-{
-if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
-else
+n++;
+
+while (n > 98)
 {
 printf("%d, ", n);
+n--;
+
 }
-}
+
+if (n == 98)
+printf("%d", n);
+printf("\n");
 }
 }
