35cf591a34040d4361d62f8f1cec04afdaaa93be
entsoereng <entsoereng@aol.com>
2023-10-25 01:18:30 -0700

    strcat

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,40 +1,20 @@
 #include "main.h"
 
 /**
-* _strlen - returns the length of a string
-* @s: string
-* Return: length
-*/
-
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-
-return (len);
-}
-
-/**
-* _strcpy - copies the string pointed to by src,
-* including the terminating null byte (\0),
-* to the buffer pointed to by dest
-* @dest: copy source to this buffer
-* @src: this is the source to copy
-* Return: copy of original source
-*/
-
+ * _strcpy -funtion copies the string pointed
+ * @dest: pointer to copy the string.
+ * @src: the string to copy to the destination
+ *
+ * Return: a copy of the src.
+ */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-
-for (i = 0; i <= _strlen(src); i++)
-dest[i] = src[i];
-
+int index = 0;
+while (*src != '\0')
+{
+dest[index] = *src;
+index++;
+src++;
+}
 return (dest);
 }
-
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+/**
+ * Prototypes of c pointers, arrays, strings
+ */
+
 int _putchar(char C);
 
 void reset_to_98(int *n);
@@ -21,3 +25,17 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
+
+
+/**
+ * Prototypes for C-more pointers, arrays and strings
+ */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
