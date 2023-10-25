28125c44b4ffccca8468d84ad7b99f07f2e7cb98
entsoereng <entsoereng@aol.com>
2023-10-25 05:59:37 -0700

    7-leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - Function that encodes a string to 1337
+ *@s: String to make 1337
+ *
+ * Return: a point the s
+ */
+char *leet(char *s)
+{
+char lt[10][2] = {
+{'a', '0' + 4}, {'A', '0' + 4},
+{'e', '0' + 3}, {'E', '0' + 3},
+{'o', '0'}, {'0', '0'},
+{'t', '0' + 7}, {'T', '0' + 7},
+{'l', '0' + 1}, {'L', '0' + 1}};
+int i = 0;
+int z = 0;
+while (s[i] != '\0')
+{
+if (s[i] == lt[z][0])
+s[i] = lt[z][1];
+z++;
+}
+z = 0;
+i++;
+}
+return (s);
+}
