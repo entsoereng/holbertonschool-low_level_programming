24136cbe8423c515e280e23d91f993c0803a3f43
entsoereng <entsoereng@aol.com>
2023-10-27 05:47:42 -0700

    rot13

---
+++ pointers_arrays_strings/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Function that checks for upper character
+ * @c: Character to be tested
+ *
+ * Return: 1 if c is upper and 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && <= 90)
+{
+return (1);
+}
+return (0);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,18 @@
+/*
+ * Prototypes of C more functions
+ */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+
 /**
  * Prototypes of c pointers, arrays, strings
  */
@@ -38,6 +53,10 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 /**
  * Prototypes of C-even more pointers, array and strings
