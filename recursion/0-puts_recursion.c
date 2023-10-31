7c3ebd65601bd31de90703d08b2d40bc050cfb9a
entsoereng <entsoereng@aol.com>
2023-10-31 00:36:41 -0700

    0-puts_recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _put_recursion - prints a string followed by a newline
+ * @s: string in which to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+if (!*s)
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion((s + 1));
+}
