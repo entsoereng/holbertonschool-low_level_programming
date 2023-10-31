625cbf63e95989e7386c92500e8797b93c463451
entsoereng <entsoereng@aol.com>
2023-10-31 03:39:43 -0700

    6-is_prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-return(prime_number(n, n - 1));
+return (prime_number(n, n - 1));
 }
 /**
  * prime_number - help function for is_prime_number
