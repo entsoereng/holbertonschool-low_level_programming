b5c13ef928e872b04e8ff0987f0d43ead644b0ac
entsoereng <entsoereng@aol.com>
2023-11-01 05:57:15 -0700

    0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: name of app + any other arguments.
+ *
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
+
