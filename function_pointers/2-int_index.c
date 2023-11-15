ed4d3f6ef06536119f34464844b7ff1d11235a0c
entsoereng <entsoereng@aol.com>
2023-11-15 01:24:38 -0800

    2-int_index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * int_index - iterate over an array
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function to execute on the array
+ *
+ * Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int index;
+index = size;
+if (size <= 0)
+return (-1);
+if (!array || !cmp)
+return (-1);
+while (size--)
+{
+if (cmp(array[index - size]))
+return (index - size);
+}
+return(-1);
+}
