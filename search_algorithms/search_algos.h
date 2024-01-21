279969015c12043d81bae46f35f823b13009dc2a
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-21 11:25:48 +0000

    Update search_algos.h

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,1 +1,46 @@
-search
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+int n;
+size_t index;
+struct listint_s *next;
+} listint_t;
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
+} skiplist_t;
+
+/* Function prototypes for various search algorithms */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H_ */
